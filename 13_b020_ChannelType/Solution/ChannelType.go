3acb9c002c44a7147718376d64a75d6dd4086681
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-11 22:18:55 +0700

    Added 202602112219

--- 13_b020_ChannelType/Solution/ChannelType.go
+++ 13_b020_ChannelType/Solution/ChannelType.go
@@ -1,31 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	var ch1 chan int
-	ch1 = make(chan int, 2)
-	ch1 <- 5
-	ch1 <- 7
-	fmt.Printf("len(ch1)=%v\n", len(ch1))
-	res1 := <-ch1
-	fmt.Printf("len(ch1)=%v | res1=%v\n", len(ch1), res1)
-	fmt.Printf("//////////////////////////////////////////////\n")
+	// var ch1 chan int
+	// ch1 = make(chan int, 2)
+	// ch1 <- 5
+	// ch1 <- 7
+	// fmt.Printf("len(ch1)=%v\n", len(ch1))
+	// res1 := <-ch1
+	// fmt.Printf("len(ch1)=%v | res1=%v\n", len(ch1), res1)
+	// fmt.Printf("//////////////////////////////////////////////\n")
 	//////////////////////////////////////////////////////////////////
-	ch2 := make(chan int, 2)
-	ch2 <- 5
-	ch2 <- 7
-	close(ch2)
-	for x := range ch2 {
-		fmt.Printf("x=%v | len(ch2)=%v\n", x, len(ch2))
-	}
-	ch2 = make(chan int, 2)
-	ch2 <- 8
-	close(ch2)
-	for x := range ch2 {
-		fmt.Printf("x=%v | len(ch2)=%v\n", x, len(ch2))
-	}
-	fmt.Printf("//////////////////////////////////////////////\n")
+	// ch2 := make(chan int, 2)
+	// ch2 <- 5
+	// ch2 <- 7
+	// close(ch2)
+	// for x := range ch2 {
+	// 	fmt.Printf("x=%v | len(ch2)=%v\n", x, len(ch2))
+	// }
+	// ch2 = make(chan int, 2)
+	// ch2 <- 8
+	// close(ch2)
+	// for x := range ch2 {
+	// 	fmt.Printf("x=%v | len(ch2)=%v\n", x, len(ch2))
+	// }
+	// fmt.Printf("//////////////////////////////////////////////\n")
 	//////////////////////////////////////////////////////////////////
-
+	// var sw sync.WaitGroup
+	// var ch1 chan string = make(chan string)
+	// sw.Add(1)
+	// go Uayfunc1(ch1, &sw)
+	// time.Sleep(5 * time.Second)
+	// ch1 <- "Hello Uaychai"
+	// sw.Wait()
+	// // ch1 <- "Hello Uaychai"
+	// sw.Add(1)
+	// go Uayfunc1(ch1, &sw)
+	// time.Sleep(5 * time.Second)
+	// ch1 <- "Hello Uaychai Again"
+	// sw.Wait()
+	// fmt.Printf("//////////////////////////////////////////////\n")
+	////////////////////////////////////////////////////////////////
+	var sw sync.WaitGroup
+	var ch1 chan string = make(chan string)
+	var ch2 chan int = make(chan int)
+	sw.Add(1)
+	go Uayfunc2(ch1, ch2, &sw)
+	time.Sleep(5 * time.Second)
+	ch1 <- "Hello Uaychai"
+	sw.Wait()
+	sw.Add(1)
+	go Uayfunc2(ch1, ch2, &sw)
+	time.Sleep(5 * time.Second)
+	ch2 <- 500
+	sw.Wait()
+	fmt.Printf("//////////////////////////////////////////////\n")
 }

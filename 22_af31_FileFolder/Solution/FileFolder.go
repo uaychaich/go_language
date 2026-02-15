a27355956c445d6dbc46375f23a51aeeaad39ed4
Uaychai Chotjaratwanich <chotjaratwanich@hotmail.com>
2026-02-15 13:28:26 +0700

    Added 202602151328

--- 22_af31_FileFolder/Solution/FileFolder.go
+++ 22_af31_FileFolder/Solution/FileFolder.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("Your input is %v", text)
+	///////////////////////////////////////////////////////////////
 	curdir, err := os.Getwd()
 	if err == nil {
 		fmt.Printf("Current Working Directory: %v\n", curdir)
